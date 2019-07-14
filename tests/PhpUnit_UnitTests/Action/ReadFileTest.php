0d7b5ca2bf9c5b93e85ad3f8981b6ac1fd0bad76
Holger Segnitz <holger@segnitz.net>
2019-07-14 16:01:39 +0200

    ADD: reading of the actual file

--- tests/PhpUnit_UnitTests/Action/ReadFileTest.php
+++ tests/PhpUnit_UnitTests/Action/ReadFileTest.php
@@ -36,93 +36,19 @@ class ReadFileTest extends TestCase
         $action->run();
     }
 
-/*
-    public function testMissingUsecaseNameCausesException(): void
+    public function testReadingAnActualFile(): void
     {
-        $action = new End($this->config);
+        $config = new Config();
+        $config->setDataPath(__DIR__ . '/../../playground/');
 
-        $this->expectException(Exception::class);
-        $this->expectExceptionCode(Exception::NAME_OF_USECASE_MISSING_IN_REQUEST);
+        $path = $config->getDataPath() . DIRECTORY_SEPARATOR . 'unittest.txt';
+        file_put_contents($path, 'ThisIsSomethingToRead');
 
-        $action->run();
+        $action = new ReadFile($config);
+        $_GET['filename'] = $path;
+
+        $this->assertEquals('ThisIsSomethingToRead', $action->run());
+
+        unlink($path);
     }
-
-    public function testInvalidUsecaseNameCausesException(): void
-    {
-        $action = new End($this->config);
-
-        $_GET['usecase'] = '@~#{}[]$%&*';
-
-        $this->expectException(Exception::class);
-        $this->expectExceptionCode(Exception::NAME_OF_USECASE_MISSING_IN_REQUEST);
-
-        $action->run();
-    }
-
-    public function testProblemWithTraceDirectoryThrowsException(): void
-    {
-        ini_set('xdebug.trace_output_dir', '/warrrggbblllgrrglllblll/');
-
-        $_GET['usecase'] = 'UnitTest';
-        $end = new End($this->config);
-
-        $this->expectException(Exception::class);
-        $this->expectExceptionCode(Exception::TRACEFILE_DIRECTORY_NOT_READABLE);
-
-        $end->run();
-    }
-
-    public function testFreshDirectory(): void
-    {
-        // prepare
-        touch(ini_get('xdebug.trace_output_dir') . DIRECTORY_SEPARATOR . 'abcdef.xt');
-        touch(ini_get('xdebug.trace_output_dir') . DIRECTORY_SEPARATOR . 'abcdef.xt.coverage');
-        touch($this->config->getDataPath() . DIRECTORY_SEPARATOR . Config::TRIGGER_FILENAME);
-
-        // secure the stuff
-        $_GET['usecase'] = 'UnitTest';
-        $end = new End($this->config);
-        $end->run();
-
-        // assert
-        $this->assertDirectoryExists($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest');
-        $this->assertFileExists($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest/abcdef.xt');
-        $this->assertFileExists($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest/abcdef.xt.coverage');
-        $this->assertFileExists($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest/' . Config::COMPLETED_FILENAME);
-
-        // cleanup
-        unlink($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest/abcdef.xt');
-        unlink($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest/abcdef.xt.coverage');
-        unlink($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest/' . Config::COMPLETED_FILENAME);
-        rmdir($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest');
-    }
-
-    public function testExistingDirectory(): void
-    {
-        // prepare
-        touch(ini_get('xdebug.trace_output_dir') . DIRECTORY_SEPARATOR . 'abcdef.xt');
-        touch(ini_get('xdebug.trace_output_dir') . DIRECTORY_SEPARATOR . 'abcdef.xt.coverage');
-        touch($this->config->getDataPath() . DIRECTORY_SEPARATOR . Config::TRIGGER_FILENAME);
-        mkdir($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest2');
-        touch($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest2/i_should_not_survive.xt');
-
-        // secure the stuff
-        $_GET['usecase'] = 'UnitTest2';
-        $end = new End($this->config);
-        $end->run();
-
-        // assert
-        $this->assertDirectoryExists($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest2');
-        $this->assertFileExists($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest2/abcdef.xt');
-        $this->assertFileExists($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest2/abcdef.xt.coverage');
-        $this->assertFileExists($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest2/' . Config::COMPLETED_FILENAME);
-        $this->assertFileNotExists($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest2/i_should_not_survive.xt');
-
-        // cleanup
-        unlink($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest2/abcdef.xt');
-        unlink($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest2/abcdef.xt.coverage');
-        unlink($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest2/' . Config::COMPLETED_FILENAME);
-        rmdir($this->config->getDataPath() . DIRECTORY_SEPARATOR . 'UnitTest2');
-    }
-*/
 }
