dd1b1cb2f7e7aaec0773f59e51350137d1474b3b
Holger Segnitz <holger@segnitz.net>
2019-07-14 21:06:39 +0200

    ADD: collection code

--- src/execute.php
+++ src/execute.php
@@ -3,7 +3,24 @@
 use Soarce\Config;
 use Soarce\FrontController;
 
-$output = (new FrontController(new Config()))->run();
-if ('' !== $output) {
-    die($output);
+$SOARCEconfig = new Config();
+$SOARCEoutput = (new FrontController($SOARCEconfig))->run();
+if ('' !== $SOARCEoutput) {
+    die($SOARCEoutput);
+}
+
+if (isset($_COOKIE['XDEBUG_TRACE']) && file_exists($SOARCEconfig->getDataPath() . DIRECTORY_SEPARATOR . Config::COMPLETED_FILENAME)) {
+    /** @noinspection ForgottenDebugOutputInspection */
+    /** @noinspection PhpComposerExtensionStubsInspection */
+    xdebug_start_code_coverage();
+
+    register_shutdown_function(static function () {
+        /** @noinspection ForgottenDebugOutputInspection */
+        /** @noinspection PhpComposerExtensionStubsInspection */
+        $outfile = xdebug_get_tracefile_name() . '.coverage';
+
+        /** @noinspection ForgottenDebugOutputInspection */
+        /** @noinspection PhpComposerExtensionStubsInspection */
+        file_put_contents($outfile, json_encode(xdebug_get_code_coverage()));
+    });
 }
