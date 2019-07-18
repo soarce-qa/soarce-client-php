7f42942e0e62073fe8a3a4005b4b26b3637d378f
Holger Segnitz <holger@segnitz.net>
2019-07-18 22:49:46 +0200

    ADD: end command without unittest

--- src/Action/End.php
+++ src/Action/End.php
@@ -8,61 +8,6 @@ use Soarce\Config;
 class End extends Action
 {
     /**
-     * creates the usecase's directory, if it exists, it is cleared out
-     *
-     * @param  string $name
-     * @return string
-     * @throws Exception
-     */
-    private function createSubdir($name): string
-    {
-        $path = $this->config->getDataPath() . DIRECTORY_SEPARATOR . $this->filterUsecase($name);
-        if (!is_dir($path)) {
-            if (!mkdir($path) && !is_dir($path)) {
-                throw new Exception(sprintf('Directory "%s" was not created', $path));
-            }
-        } else {
-            $dirIter = new \DirectoryIterator($path);
-            foreach ($dirIter as $file) {
-                if ($file->isFile()) {
-                    unlink($file->getRealPath());
-                }
-            }
-        }
-        return $path;
-    }
-
-    /**
-     * @param  string $fullPathDestination
-     * @throws Exception
-     * @return int
-     */
-    private function moveFiles($fullPathDestination): int
-    {
-        // traces
-        $traceFileDirectory = trim(ini_get('xdebug.trace_output_dir'));
-        if ('' === $traceFileDirectory || '/' === $traceFileDirectory || !is_readable($traceFileDirectory)) {
-            throw new Exception('Tracefile Directory does not exist or is not readable.', Exception::TRACEFILE_DIRECTORY_NOT_READABLE);
-        }
-
-        $count = 0;
-        $dirIter = new \DirectoryIterator($traceFileDirectory);
-        foreach ($dirIter as $file) {
-            if ($file->isDir() || ($file->getExtension() !== Config::SUFFIX_TRACEFILE && $file->getExtension() !== Config::SUFFIX_COVERAGEFILE)) {
-                continue;
-            }
-
-            rename(
-                $file->getRealPath(),
-                $fullPathDestination . DIRECTORY_SEPARATOR . $file->getFilename()
-            );
-            ++$count;
-        }
-
-        return $count;
-    }
-
-    /**
      * @return string
      * @throws Exception
      */
@@ -72,15 +17,31 @@ class End extends Action
             throw new Exception('data dir does not exist, is not writable or full', Exception::DATA_DIRECTORY__NOT_WRITEABLE);
         }
 
-        if (!isset($_GET['usecase']) || '' === trim($_GET['usecase']) || '' === $this->filterUsecase($_GET['usecase'])) {
-            throw new Exception('name of usecase is missing from request', Exception::NAME_OF_USECASE_MISSING_IN_REQUEST);
+        $this->writeKillfile();
+        $this->deletePipes();
+
+        return json_encode(['status' => 'ok']);
+    }
+
+    /**
+     *
+     */
+    private function writeKillfile(): void
+    {
+        $file = $this->config->getDataPath() . DIRECTORY_SEPARATOR . Config::KILL_WORKER_FILENAME;
+        if (file_exists($file)) {
+            unlink($file);
         }
+    }
 
-        $fullPath = $this->createSubdir($_GET['usecase']);
-        $count = $this->moveFiles($fullPath);
-        touch($fullPath . DIRECTORY_SEPARATOR . Config::COMPLETED_FILENAME);
-        unlink($this->config->getDataPath() . DIRECTORY_SEPARATOR . Config::TRIGGER_FILENAME);
-
-        return json_encode(['files' => $count]);
+    /**
+     * 
+     */
+    private function deletePipes(): void
+    {
+        for ($i = 0; $i < $this->config->getNumberOfPipes(); $i++) {
+            $path = $this->config->getDataPath() . DIRECTORY_SEPARATOR . sprintf(Config::PIPE_NAME_TEMPLATE, $i);
+            unlink($path);
+        }
     }
 }
