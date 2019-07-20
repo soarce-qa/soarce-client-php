1ba6294cb512f8c110d2f26836867eeaed2bb9a1
Holger Segnitz <holger@segnitz.net>
2019-07-20 14:27:36 +0200

    FIX: type param and add artificial request id

--- src/execute.php
+++ src/execute.php
@@ -9,14 +9,17 @@ if ('' !== $SOARCEoutput) {
     die($SOARCEoutput);
 }
 
+define('SOARCE_REQUEST_ID', bin2hex(random_bytes(16)));
+
 if ($SOARCEconfig->isTracingActive()) {
+    //@todo figure out the filename to use by evaluating the locks
     $SOARCEpath = $SOARCEconfig->getDataPath() . DIRECTORY_SEPARATOR . sprintf(Config::PIPE_NAME_TEMPLATE, 0);
 
     $SOARCEfp = fopen($SOARCEpath, 'wb');
     fwrite($SOARCEfp, json_encode([
         'type' => 'trace',
         'request_time' => microtime(true),
-        'request_id' => 'filled-later',
+        'request_id' => SOARCE_REQUEST_ID,
         'get'  => $_GET,
         'post' => $_POST,
         'server' => $_SERVER,
@@ -31,7 +34,7 @@ if ($SOARCEconfig->isTracingActive()) {
 
     /** @noinspection ForgottenDebugOutputInspection */
     /** @noinspection PhpComposerExtensionStubsInspection */
-    xdebug_start_trace(    //@todo figure out the filename to use by evaluating the locks
+    xdebug_start_trace(
         $SOARCEpath,
         XDEBUG_TRACE_COMPUTERIZED
     );
@@ -48,9 +51,9 @@ if ($SOARCEconfig->isTracingActive()) {
         /** @noinspection ForgottenDebugOutputInspection */
         /** @noinspection PhpComposerExtensionStubsInspection */
         file_put_contents($outfile, json_encode([
-                'type' => 'trace',
+                'type' => 'coverage',
                 'request_time' => microtime(true),
-                'request_id' => 'filled-later',
+                'request_id' => SOARCE_REQUEST_ID,
                 'get'  => $_GET,
                 'post' => $_POST,
                 'server' => $_SERVER,
