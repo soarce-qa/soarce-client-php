558285fad80325f23a26b936f0bafa6f9651b1fa
Holger Segnitz <holger@segnitz.net>
2019-07-21 12:53:05 +0200

    FIX: possible racecondition in reserving coverage file

--- src/execute.php
+++ src/execute.php
@@ -23,11 +23,13 @@ define('SOARCE_REQUEST_ID', bin2hex(random_bytes(16))); //TODO implement request
 if ($config->isTracingActive()) {
     $pipeHandler = new Handler($config);
     $tracePipe = $pipeHandler->getFreePipe();
+    $coveragePipe = $pipeHandler->getFreePipe();
+    $requestTime = microtime(true);
 
     $fpTracefile = fopen($tracePipe->getFilenameTracefile(), 'wb');
     fwrite($fpTracefile, json_encode([
             'type' => 'trace',
-            'request_time' => microtime(true),
+            'request_time' => $requestTime,
             'request_id' => SOARCE_REQUEST_ID,
             'get'  => $_GET,
             'post' => $_POST,
@@ -42,20 +44,20 @@ if ($config->isTracingActive()) {
         XDEBUG_TRACE_COMPUTERIZED
     );
 
-    register_shutdown_function(static function () use ($pipeHandler){
+    register_shutdown_function(static function () use ($coveragePipe, $requestTime){
         xdebug_stop_trace();
 
-        $coveragePipe = $pipeHandler->getFreePipe();
-
-        file_put_contents($coveragePipe->getFilenameTracefile(), json_encode([
+        $fp = fopen($coveragePipe->getFilenameTracefile(), 'wb');
+        fwrite($fp, json_encode([
                 'type' => 'coverage',
-                'request_time' => microtime(true),
+                'request_time' => $requestTime,
                 'request_id' => SOARCE_REQUEST_ID,
                 'get'  => $_GET,
                 'post' => $_POST,
                 'server' => $_SERVER,
                 'env' => $_ENV,
-            ]) . "\n" . json_encode(xdebug_get_code_coverage())
-        );
+            ]) . "\n");
+        fwrite($fp, json_encode(xdebug_get_code_coverage()) . "\n");
+        fclose($fp);
     });
 }
