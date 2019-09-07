2c6c9cea78b761388b2eb10e5fe30cfbfe020217
Holger Segnitz <holger@segnitz.net>
2019-09-07 19:19:19 +0200

    ADD: HashManager UnitTest

---
+++ tests/PhpUnit_UnitTests/HashManagerTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace UnitTests;
+
+use M6Web\Component\RedisMock\RedisMockFactory;
+use PHPUnit\Framework\TestCase;
+use Predis\Client;
+use Predis\ClientInterface;
+use Soarce\HashManager;
+
+class HashManagerTest extends TestCase
+{
+    private const REDIS_KEY = 'filehashes:testApplicationName';
+
+    /** @var ClientInterface */
+    private $redisMock;
+
+    public function testStoreNewFileInEmptyCache(): void
+    {
+        $filename = realpath(__DIR__ . '/Fixtures/dummy.txt');
+
+        $hashManager = $this->getPreparedInstance();
+        $this->assertEquals(
+            '54b0c58c7ce9f2a8b551351102ee0938',
+            $hashManager->getMd5ForFile($filename)
+        );
+
+        $hashManager->save();
+
+        $storageContent = $this->redisMock->hgetall(self::REDIS_KEY);
+        $this->assertCount(1, $storageContent);
+        $this->assertEquals('54b0c58c7ce9f2a8b551351102ee0938', array_pop($storageContent));
+    }
+
+    public function testCaching(): void
+    {
+        $filename = realpath(__DIR__ . '/Fixtures/dummy.txt');
+
+        $hashManager = $this->getPreparedInstance();
+
+        $this->redisMock->hset(self::REDIS_KEY, $filename, 'thisAintNoMd5Hash');
+
+        $hashManager->load();
+
+        $this->assertEquals(
+            [$filename => 'thisAintNoMd5Hash'],
+            $hashManager->getMd5ForFiles([$filename])
+        );
+    }
+
+    public function testEvalIsIgnored(): void
+    {
+        $filename = "something in eval()'d code we do not want";
+
+        $hashManager = $this->getPreparedInstance();
+        $this->assertEquals(
+            '',
+            $hashManager->getMd5ForFile($filename)
+        );
+    }
+
+    /**
+     * @return HashManager
+     */
+    private function getPreparedInstance(): HashManager
+    {
+        $factory   = new RedisMockFactory();
+
+        /** @var ClientInterface $redisMock */
+        $this->redisMock = $factory->getAdapter(Client::class, true);
+
+        return new HashManager($this->redisMock, 'testApplicationName');
+    }
+}
