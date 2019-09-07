a04533a7018408b8ec7851af0f95a12ffcb58b18
Holger Segnitz <holger@segnitz.net>
2019-09-07 19:19:00 +0200

    ADD: test for ping

---
+++ tests/PhpUnit_UnitTests/Action/PingTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace UnitTests\Action;
+
+use PHPUnit\Framework\TestCase;
+use Soarce\Action\Ping;
+use Soarce\Config;
+
+class PingTest extends TestCase
+{
+    public function testPongIsReceived(): void
+    {
+        $action = new Ping(new Config());
+        $this->assertEquals('pong', $action->run());
+    }
+}
